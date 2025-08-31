d3a039f3da67494c9df4a6dc9bcb28f26b575b43
anandanpm <ananda1732001@gmail.com>
2025-08-31 12:14:25 +0530

    (Ticket_Model):Adding the TicketModel

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,14 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "../models/User"
+import { Ticket } from "../models/Ticket"
 
 const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/event_management"
 
 export const AppDataSource = new DataSource({
   type: "mongodb",
   url: MONGO_URL,
-  entities: [User],
+  entities: [User,Ticket],
   synchronize: true, // For dev/demo. Use migrations in production.
   logging: false,
 })
