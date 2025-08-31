ad8b9db1867b6e39f5753d264a65afd132d091e2
anandanpm <ananda1732001@gmail.com>
2025-08-31 12:16:23 +0530

    (Booking_Model):Adding the BookingModel

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,13 +2,14 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "../models/User"
 import { Ticket } from "../models/Ticket"
+import { Event } from "../models/Event"
+import { Booking } from "../models/Booking"
 
 const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/event_management"
 
 export const AppDataSource = new DataSource({
   type: "mongodb",
   url: MONGO_URL,
-  entities: [User,Ticket],
-  synchronize: true, // For dev/demo. Use migrations in production.
+  entities: [User,Ticket,Event,Booking],
   logging: false,
 })
