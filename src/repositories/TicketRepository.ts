b4e61c603f6d6e387a70a9d1b68d186f1a4124a3
anandanpm <ananda1732001@gmail.com>
2025-08-31 12:36:38 +0530

    (Ticket_Repository):Adding the TicketRepository

---
+++ src/repositories/TicketRepository.ts
@@ -0,0 +1,31 @@
+import { injectable } from "tsyringe"
+import { AppDataSource } from "../config/database"
+import { Ticket } from "../models/Ticket"
+import type { MongoRepository } from "typeorm"
+import type { ObjectId } from "mongodb"
+
+@injectable()
+export class TicketRepository {
+  private repo: MongoRepository<Ticket>
+  constructor() {
+    this.repo = AppDataSource.getMongoRepository(Ticket)
+  }
+
+  async create(data: Partial<Ticket>) {
+    const doc = this.repo.create(data)
+    return this.repo.save(doc)
+  }
+
+  async update(id: ObjectId, data: Partial<Ticket>) {
+    await this.repo.update(id, data)
+    return this.findById(id)
+  }
+
+  async findById(id: ObjectId) {
+    return this.repo.findOne({ where: { _id: id } })
+  }
+
+  async listByEvent(eventId: ObjectId) {
+    return this.repo.find({ where: { eventId } })
+  }
+}
