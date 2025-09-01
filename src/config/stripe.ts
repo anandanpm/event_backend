65d55edf9e31963d464c1dba631f4b4b8ced8672
anandanpm <ananda1732001@gmail.com>
2025-09-02 03:39:57 +0530

    (Stripe):change in the stripe

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -5,7 +5,7 @@ dotenv.config()
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY as string
 
 export const stripe = new Stripe(STRIPE_SECRET_KEY, {
-  apiVersion: "2023-10-16",
+  apiVersion: '2024-12-18.acacia' as any,
 })
 
 export const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || ""
