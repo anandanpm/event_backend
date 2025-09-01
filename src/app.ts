ddb719856bb87935d4c1b9dcae953b0415044d5e
anandanpm <ananda1732001@gmail.com>
2025-09-02 01:40:19 +0530

    (Added frontend url):Added the url

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
 
 
     app.use(cors({
-      origin: 'http://localhost:5173',
+      origin: ['http://localhost:5173','https://event-frontend-line.vercel.app/'],
       credentials: true, 
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization'],
