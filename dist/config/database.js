bcd65b6c7f4763b97430b70d6652213c0c873d49
anandanpm <ananda1732001@gmail.com>
2025-09-02 01:59:39 +0530

    (Dist_folder2):create the distfolder

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,4 +1,7 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppDataSource = void 0;
 require("reflect-metadata");
@@ -7,7 +10,9 @@ const User_1 = require("../models/User");
 const Ticket_1 = require("../models/Ticket");
 const Event_1 = require("../models/Event");
 const Booking_1 = require("../models/Booking");
-const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/event_management";
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const MONGO_URL = process.env.MONGO_URL;
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "mongodb",
     url: MONGO_URL,
