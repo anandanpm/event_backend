ce8f142fb3cb8c81072af1752c898f7e64ca6fca
anandanpm <ananda1732001@gmail.com>
2025-09-02 10:05:19 +0530

    (Dist_folder6):Changing the dist file

--- dist/dto/ticket/TicketResponseDto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TicketResponseDto = void 0;
-class TicketResponseDto {
-}
-exports.TicketResponseDto = TicketResponseDto;
-//# sourceMappingURL=TicketResponseDto.js.map
