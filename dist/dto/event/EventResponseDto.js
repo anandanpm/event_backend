54b61d05799530037eac5d0d74a89809fa7d054f
anandanpm <ananda1732001@gmail.com>
2025-09-02 11:11:18 +0530

    (dist_folder):Changes done

--- dist/dto/event/EventResponseDto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EventResponseDto = void 0;
-class EventResponseDto {
-}
-exports.EventResponseDto = EventResponseDto;
-//# sourceMappingURL=EventResponseDto.js.map
--- dist/interfaces/repositories/ITicketRepository.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=ITicketRepository.js.map
